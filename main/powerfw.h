402f70f68a4a33c824eac6279a9e05b4c018ae84
lockzane2014 <yt.lockzane@gmail.com>
2026-07-03 14:27:26 +0200

    Update powerfw.h

--- main/powerfw.h
+++ main/powerfw.h
@@ -1,1 +1,14 @@
+#ifndef POWERFW_H
+#define POWERFW_H
 
+#include "esp_err.h"
+
+// Function declarations
+esp_err_t powerfw_init(void);
+
+// Menu functions
+void menu_start(void);
+
+// Add more later...
+
+#endif // POWERFW_H
