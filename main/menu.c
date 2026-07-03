721ec0c687950d8eb5fe4fcf1b83c36e01e67ce3
lockzane2014 <yt.lockzane@gmail.com>
2026-07-03 14:28:00 +0200

    Update menu.c

--- main/menu.c
+++ main/menu.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "menu.h"
+#include "powerfw.h"
 
+static void menu_task(void *pvParameters)
+{
+    while (1) {
+        printf("PowerFW Menu - Waiting for implementation...\n");
+        // TODO: Add button input + display later
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+void menu_start(void)
+{
+    xTaskCreate(menu_task, "menu_task", 4096, NULL, 5, NULL);
+    printf("Menu started!\n");
+}
