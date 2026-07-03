f53d8462b60dfd2dc6411325071832c022ecbf88
lockzane2014 <yt.lockzane@gmail.com>
2026-07-03 14:29:18 +0200

    Update cc1101.c

--- main/cc1101.c
+++ main/cc1101.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include "cc1101.h"
+#include "driver/spi_master.h"
 
+esp_err_t cc1101_init(void)
+{
+    printf("CC1101 Sub-GHz radio initializing...\n");
+    // TODO: Full SPI + CC1101 register setup
+    return ESP_OK;
+}
+
+void cc1101_transmit(uint8_t *data, size_t len)
+{
+    printf("Transmitting %d bytes via CC1101\n", len);
+    // TODO: Implement real transmit
+}
+
+void cc1101_receive(void)
+{
+    printf("Listening on CC1101...\n");
+    // TODO: Implement RX
+}
